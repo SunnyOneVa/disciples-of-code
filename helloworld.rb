30f7b1d1d2692c17cb867c0d8fe90ec78d192cd2
SunnyOneVa <bydawnslight@gmail.com>
2014-04-02 22:57:23 -0400

    Added helloworld.rb
    My first Ruby program!

---
+++ helloworld.rb
@@ -0,0 +1,12 @@
+puts("Hello, world!")
+greeting = "Hello"
+punctuation = ", "
+subject = "Dawn!"
+print(greeting, punctuation, subject)
+
+prompt = "What is your name? "
+print prompt
+name = gets
+print(greeting, punctuation, name, '\n')
+puts("Thanks for using my first program!")
+
